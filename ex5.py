453586283873098389467fa07fb5c3ca34704e82
Guest <jones.jasiah073@gmail.com>
2016-06-26 00:27:38 -0400

    Adding exercise files.

---
+++ ex5.py
@@ -0,0 +1,12 @@
+my _name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "Lets talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my _weight
+print 
